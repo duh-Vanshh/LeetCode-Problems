6409a827eef546535b7d0d914b0174f913c6cfad
Vansh Verma <vanshshadow19@gmail.com>
2026-07-19 16:55:29 +0530

    Time: 46 ms (15.23%), Space: 54.2 MB (83.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,8 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length - 1; i++) {
-        for(let j = i+1; j < nums.length; j++) {
+    let n = nums.length
+    for(let i = 0; i < n - 1; i++) {
+        for(let j = i+1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
                 return [i, j]
             }
